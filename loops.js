a34514d3ea14461bc87e6051c7e8061ec4525df6
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-03-11 04:16:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,12 @@
 function forLoop(array) {
-  for(i = 0; i < 25; i++)
+  for(i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("i am 1 strange loop.")
     } else {
       array.push(`I am ${i} strange loops.`)
     }
+  }  
+    return array;
 }
-return array;
+
+
