f653f14421d72e12a5d6e635b62f2ba75a82af1a
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-03-11 04:14:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,6 @@ function forLoop(array) {
     if (i === 1) {
       array.push("i am 1 strange loop.")
     } else {
-      array.push(`I am ${i} strange loops.`
-)
+      array.push(`I am ${i} strange loops.`)
     }
 }
